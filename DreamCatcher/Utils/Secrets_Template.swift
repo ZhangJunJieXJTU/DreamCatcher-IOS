f94258bbdd2db9464a8e403465e6c43369f7ccd8
zhangjunjie <j19834739958@163.com>
2025-12-28 16:27:10 +0800

    Fix Secrets redeclaration error by renaming template enum

--- DreamCatcher/Utils/Secrets_Template.swift
+++ DreamCatcher/Utils/Secrets_Template.swift
@@ -1,4 +1,5 @@
-enum Secrets {
+enum Secrets_Template {
     static let openAIKey = "YOUR_OPENAI_KEY_HERE"
     static let baseURL = "https://api.openai.com/v1"
 }
+// Usage: Rename this file to Secrets.swift and change 'Secrets_Template' to 'Secrets'
